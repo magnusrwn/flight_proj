c867039182bb95d4a656fd056a5e0b3b197843b6
magnusrwn <119822862+magnusrwn@users.noreply.github.com>
2026-07-28 20:40:50 +0200

    Removed temp file -- forgot to previously

--- backend/src/ml/mac.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-PATH_TO_CSV = Path(__file__).resolve().parents[2]/"data/csv/flight_data_2024_sample.csv"
-
-
-r = pd.read_csv(PATH_TO_CSV)
-print(r.columns)
-
