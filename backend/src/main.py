f9f824b49be22567e4bcd6196b1ebd78214f7a63
magnusrwn <119822862+magnusrwn@users.noreply.github.com>
2026-08-02 19:59:59 +0200

    creating API for requests. FlightAPI purchased, so info can be got on flights. Making the tests for the endpoint now, so a schema is easy to integrate with logic
    Good work on making this test. The first of many. It was quite fun tbh

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from logger_config import configure_logging
-
-
 configure_logging()
 
 app = FastAPI()
+
+# Response Model (Would define in ~/models, however just one endpoint here)
+@app.get("/health")
+def health_check():
+    return {"status":"ok"}
+
+@app.post("/send-flight-number")
+def send_flight_number(body):
+    pass
